6db7fad5069867329f889de277c291de957d80ec
Jose Doval <dovaldam108@gmail.com>
2020-10-09 01:53:27 +0200

    empezamos la practica 01 Extra 2

---
+++ Practica01 Extra 02 Jose Angel Doval Fraga1/Practica01 Extra 02 Jose Angel Doval Fraga1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica01_Extra_02_Jose_Angel_Doval_Fraga1
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Practica01());
+        }
+    }
+}
