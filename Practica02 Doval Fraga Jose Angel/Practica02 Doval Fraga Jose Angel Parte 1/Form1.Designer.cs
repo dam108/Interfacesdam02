59e86d7cc2a6d10aef79c7434a74800ffa872021
Jose Doval <dovaldam108@gmail.com>
2020-10-12 20:41:26 +0200

    Estamos en la primera parte en el boton añadir

--- Practica02 Doval Fraga Jose Angel/Practica02 Doval Fraga Jose Angel Parte 1/Form1.Designer.cs
+++ Practica02 Doval Fraga Jose Angel/Practica02 Doval Fraga Jose Angel Parte 1/Form1.Designer.cs
@@ -29,22 +29,23 @@
         private void InitializeComponent()
         {
             this.gBox_insertar = new System.Windows.Forms.GroupBox();
-            this.gBox_salida = new System.Windows.Forms.GroupBox();
-            this.lbl_modelo = new System.Windows.Forms.Label();
-            this.lbl_color = new System.Windows.Forms.Label();
-            this.lbl_ancho = new System.Windows.Forms.Label();
-            this.lbl_cm = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.lbl_alto = new System.Windows.Forms.Label();
-            this.txtModelo = new System.Windows.Forms.TextBox();
-            this.txtColor = new System.Windows.Forms.TextBox();
-            this.txtAncho = new System.Windows.Forms.TextBox();
-            this.txtAlto = new System.Windows.Forms.TextBox();
             this.btnAñadir = new System.Windows.Forms.Button();
-            this.cboAlfombras = new System.Windows.Forms.ComboBox();
-            this.btnInfoAlfombra = new System.Windows.Forms.Button();
-            this.btnEliminarAlfombra = new System.Windows.Forms.Button();
+            this.txtAlto = new System.Windows.Forms.TextBox();
+            this.txtAncho = new System.Windows.Forms.TextBox();
+            this.txtColor = new System.Windows.Forms.TextBox();
+            this.txtModelo = new System.Windows.Forms.TextBox();
+            this.lbl_alto = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_cm = new System.Windows.Forms.Label();
+            this.lbl_ancho = new System.Windows.Forms.Label();
+            this.lbl_color = new System.Windows.Forms.Label();
+            this.lbl_modelo = new System.Windows.Forms.Label();
+            this.gBox_salida = new System.Windows.Forms.GroupBox();
             this.btnEliminarTodas = new System.Windows.Forms.Button();
+            this.btnEliminarAlfombra = new System.Windows.Forms.Button();
+            this.btnInfoAlfombra = new System.Windows.Forms.Button();
+            this.cboAlfombras = new System.Windows.Forms.ComboBox();
+            this.lblAdvertencia = new System.Windows.Forms.Label();
             this.gBox_insertar.SuspendLayout();
             this.gBox_salida.SuspendLayout();
             this.SuspendLayout();
@@ -67,70 +68,56 @@
             this.gBox_insertar.Name = "gBox_insertar";
             this.gBox_insertar.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.gBox_insertar.Size = new System.Drawing.Size(995, 162);
-            this.gBox_insertar.TabIndex = 0;
+            this.gBox_insertar.TabIndex = 1;
             this.gBox_insertar.TabStop = false;
             this.gBox_insertar.Text = "Nova alfombra";
-            this.gBox_insertar.Enter += new System.EventHandler(this.gBox_insertar_Enter);
             // 
-            // gBox_salida
+            // btnAñadir
             // 
-            this.gBox_salida.Controls.Add(this.btnEliminarTodas);
-            this.gBox_salida.Controls.Add(this.btnEliminarAlfombra);
-            this.gBox_salida.Controls.Add(this.btnInfoAlfombra);
-            this.gBox_salida.Controls.Add(this.cboAlfombras);
-            this.gBox_salida.Location = new System.Drawing.Point(13, 214);
-            this.gBox_salida.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.gBox_salida.Name = "gBox_salida";
-            this.gBox_salida.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.gBox_salida.Size = new System.Drawing.Size(995, 162);
-            this.gBox_salida.TabIndex = 0;
-            this.gBox_salida.TabStop = false;
-            this.gBox_salida.Text = "Alfombras dipoñibles";
+            this.btnAñadir.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAñadir.Location = new System.Drawing.Point(759, 41);
+            this.btnAñadir.Name = "btnAñadir";
+            this.btnAñadir.Size = new System.Drawing.Size(203, 28);
+            this.btnAñadir.TabIndex = 5;
+            this.btnAñadir.Text = "Engadir";
+            this.btnAñadir.UseVisualStyleBackColor = true;
+            this.btnAñadir.Click += new System.EventHandler(this.btnAñadir_Click);
             // 
-            // lbl_modelo
+            // txtAlto
             // 
-            this.lbl_modelo.AutoSize = true;
-            this.lbl_modelo.Location = new System.Drawing.Point(28, 44);
-            this.lbl_modelo.Name = "lbl_modelo";
-            this.lbl_modelo.Size = new System.Drawing.Size(63, 21);
-            this.lbl_modelo.TabIndex = 0;
-            this.lbl_modelo.Text = "Modelo";
+            this.txtAlto.Location = new System.Drawing.Point(591, 92);
+            this.txtAlto.Name = "txtAlto";
+            this.txtAlto.Size = new System.Drawing.Size(88, 29);
+            this.txtAlto.TabIndex = 4;
+            this.txtAlto.Enter += new System.EventHandler(this.txtAlto_Enter);
+            this.txtAlto.Leave += new System.EventHandler(this.txtAlto_Leave);
             // 
-            // lbl_color
+            // txtAncho
             // 
-            this.lbl_color.AutoSize = true;
-            this.lbl_color.Location = new System.Drawing.Point(28, 100);
-            this.lbl_color.Name = "lbl_color";
-            this.lbl_color.Size = new System.Drawing.Size(35, 21);
-            this.lbl_color.TabIndex = 0;
-            this.lbl_color.Text = "Cor";
+            this.txtAncho.Location = new System.Drawing.Point(379, 92);
+            this.txtAncho.Name = "txtAncho";
+            this.txtAncho.Size = new System.Drawing.Size(88, 29);
+            this.txtAncho.TabIndex = 3;
+            this.txtAncho.Enter += new System.EventHandler(this.txtAncho_Enter);
+            this.txtAncho.Leave += new System.EventHandler(this.txtAncho_Leave);
             // 
-            // lbl_ancho
+            // txtColor
             // 
-            this.lbl_ancho.AutoSize = true;
-            this.lbl_ancho.Location = new System.Drawing.Point(319, 100);
-            this.lbl_ancho.Name = "lbl_ancho";
-            this.lbl_ancho.Size = new System.Drawing.Size(54, 21);
-            this.lbl_ancho.TabIndex = 0;
-            this.lbl_ancho.Text = "Ancho";
+            this.txtColor.Location = new System.Drawing.Point(130, 92);
+            this.txtColor.Name = "txtColor";
+            this.txtColor.Size = new System.Drawing.Size(150, 29);
+            this.txtColor.TabIndex = 2;
+            this.txtColor.Enter += new System.EventHandler(this.txtColor_Enter);
+            this.txtColor.Leave += new System.EventHandler(this.txtColor_Leave);
             // 
-            // lbl_cm
+            // txtModelo
             // 
-            this.lbl_cm.AutoSize = true;
-            this.lbl_cm.Location = new System.Drawing.Point(473, 100);
-            this.lbl_cm.Name = "lbl_cm";
-            this.lbl_cm.Size = new System.Drawing.Size(41, 21);
-            this.lbl_cm.TabIndex = 0;
-            this.lbl_cm.Text = "(cm)";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(685, 100);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(41, 21);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "(cm)";
+            this.txtModelo.Location = new System.Drawing.Point(130, 41);
+            this.txtModelo.Name = "txtModelo";
+            this.txtModelo.Size = new System.Drawing.Size(596, 29);
+            this.txtModelo.TabIndex = 1;
+            this.txtModelo.Enter += new System.EventHandler(this.txtModelo_Enter);
+            this.txtModelo.Leave += new System.EventHandler(this.txtModelo_Leave);
             // 
             // lbl_alto
             // 
@@ -141,72 +128,65 @@
             this.lbl_alto.TabIndex = 0;
             this.lbl_alto.Text = "Alto";
             // 
-            // txtModelo
+            // label1
             // 
-            this.txtModelo.Location = new System.Drawing.Point(130, 41);
-            this.txtModelo.Name = "txtModelo";
-            this.txtModelo.Size = new System.Drawing.Size(596, 29);
-            this.txtModelo.TabIndex = 2;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(685, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "(cm)";
             // 
-            // txtColor
+            // lbl_cm
             // 
-            this.txtColor.Location = new System.Drawing.Point(130, 92);
-            this.txtColor.Name = "txtColor";
-            this.txtColor.Size = new System.Drawing.Size(150, 29);
-            this.txtColor.TabIndex = 2;
+            this.lbl_cm.AutoSize = true;
+            this.lbl_cm.Location = new System.Drawing.Point(473, 100);
+            this.lbl_cm.Name = "lbl_cm";
+            this.lbl_cm.Size = new System.Drawing.Size(41, 21);
+            this.lbl_cm.TabIndex = 0;
+            this.lbl_cm.Text = "(cm)";
             // 
-            // txtAncho
+            // lbl_ancho
             // 
-            this.txtAncho.Location = new System.Drawing.Point(379, 92);
-            this.txtAncho.Name = "txtAncho";
-            this.txtAncho.Size = new System.Drawing.Size(88, 29);
-            this.txtAncho.TabIndex = 2;
+            this.lbl_ancho.AutoSize = true;
+            this.lbl_ancho.Location = new System.Drawing.Point(319, 100);
+            this.lbl_ancho.Name = "lbl_ancho";
+            this.lbl_ancho.Size = new System.Drawing.Size(54, 21);
+            this.lbl_ancho.TabIndex = 0;
+            this.lbl_ancho.Text = "Ancho";
             // 
-            // txtAlto
+            // lbl_color
             // 
-            this.txtAlto.Location = new System.Drawing.Point(591, 92);
-            this.txtAlto.Name = "txtAlto";
-            this.txtAlto.Size = new System.Drawing.Size(88, 29);
-            this.txtAlto.TabIndex = 2;
+            this.lbl_color.AutoSize = true;
+            this.lbl_color.Location = new System.Drawing.Point(28, 100);
+            this.lbl_color.Name = "lbl_color";
+            this.lbl_color.Size = new System.Drawing.Size(35, 21);
+            this.lbl_color.TabIndex = 0;
+            this.lbl_color.Text = "Cor";
             // 
-            // btnAñadir
+            // lbl_modelo
             // 
-            this.btnAñadir.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAñadir.Location = new System.Drawing.Point(759, 41);
-            this.btnAñadir.Name = "btnAñadir";
-            this.btnAñadir.Size = new System.Drawing.Size(203, 28);
-            this.btnAñadir.TabIndex = 3;
-            this.btnAñadir.Text = "Engadir";
-            this.btnAñadir.UseVisualStyleBackColor = true;
+            this.lbl_modelo.AutoSize = true;
+            this.lbl_modelo.Location = new System.Drawing.Point(28, 44);
+            this.lbl_modelo.Name = "lbl_modelo";
+            this.lbl_modelo.Size = new System.Drawing.Size(63, 21);
+            this.lbl_modelo.TabIndex = 0;
+            this.lbl_modelo.Text = "Modelo";
             // 
-            // cboAlfombras
+            // gBox_salida
             // 
-            this.cboAlfombras.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cboAlfombras.FormattingEnabled = true;
-            this.cboAlfombras.Location = new System.Drawing.Point(32, 46);
-            this.cboAlfombras.Name = "cboAlfombras";
-            this.cboAlfombras.Size = new System.Drawing.Size(930, 25);
-            this.cboAlfombras.TabIndex = 0;
-            // 
-            // btnInfoAlfombra
-            // 
-            this.btnInfoAlfombra.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnInfoAlfombra.Location = new System.Drawing.Point(32, 99);
-            this.btnInfoAlfombra.Name = "btnInfoAlfombra";
-            this.btnInfoAlfombra.Size = new System.Drawing.Size(341, 28);
-            this.btnInfoAlfombra.TabIndex = 3;
-            this.btnInfoAlfombra.Text = "Información da alfombra";
-            this.btnInfoAlfombra.UseVisualStyleBackColor = true;
-            // 
-            // btnEliminarAlfombra
-            // 
-            this.btnEliminarAlfombra.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEliminarAlfombra.Location = new System.Drawing.Point(379, 99);
-            this.btnEliminarAlfombra.Name = "btnEliminarAlfombra";
-            this.btnEliminarAlfombra.Size = new System.Drawing.Size(203, 28);
-            this.btnEliminarAlfombra.TabIndex = 3;
-            this.btnEliminarAlfombra.Text = "Eliminar alfombra";
-            this.btnEliminarAlfombra.UseVisualStyleBackColor = true;
+            this.gBox_salida.Controls.Add(this.btnEliminarTodas);
+            this.gBox_salida.Controls.Add(this.btnEliminarAlfombra);
+            this.gBox_salida.Controls.Add(this.btnInfoAlfombra);
+            this.gBox_salida.Controls.Add(this.cboAlfombras);
+            this.gBox_salida.Location = new System.Drawing.Point(13, 255);
+            this.gBox_salida.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gBox_salida.Name = "gBox_salida";
+            this.gBox_salida.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gBox_salida.Size = new System.Drawing.Size(995, 162);
+            this.gBox_salida.TabIndex = 2;
+            this.gBox_salida.TabStop = false;
+            this.gBox_salida.Text = "Alfombras dipoñibles";
             // 
             // btnEliminarTodas
             // 
@@ -218,12 +198,50 @@
             this.btnEliminarTodas.Text = "Eliminar todas";
             this.btnEliminarTodas.UseVisualStyleBackColor = true;
             // 
+            // btnEliminarAlfombra
+            // 
+            this.btnEliminarAlfombra.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminarAlfombra.Location = new System.Drawing.Point(379, 99);
+            this.btnEliminarAlfombra.Name = "btnEliminarAlfombra";
+            this.btnEliminarAlfombra.Size = new System.Drawing.Size(203, 28);
+            this.btnEliminarAlfombra.TabIndex = 2;
+            this.btnEliminarAlfombra.Text = "Eliminar alfombra";
+            this.btnEliminarAlfombra.UseVisualStyleBackColor = true;
+            // 
+            // btnInfoAlfombra
+            // 
+            this.btnInfoAlfombra.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInfoAlfombra.Location = new System.Drawing.Point(32, 99);
+            this.btnInfoAlfombra.Name = "btnInfoAlfombra";
+            this.btnInfoAlfombra.Size = new System.Drawing.Size(341, 28);
+            this.btnInfoAlfombra.TabIndex = 1;
+            this.btnInfoAlfombra.Text = "Información da alfombra";
+            this.btnInfoAlfombra.UseVisualStyleBackColor = true;
+            // 
+            // cboAlfombras
+            // 
+            this.cboAlfombras.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboAlfombras.FormattingEnabled = true;
+            this.cboAlfombras.Location = new System.Drawing.Point(32, 46);
+            this.cboAlfombras.Name = "cboAlfombras";
+            this.cboAlfombras.Size = new System.Drawing.Size(930, 25);
+            this.cboAlfombras.TabIndex = 0;
+            // 
+            // lblAdvertencia
+            // 
+            this.lblAdvertencia.Location = new System.Drawing.Point(13, 205);
+            this.lblAdvertencia.Name = "lblAdvertencia";
+            this.lblAdvertencia.Size = new System.Drawing.Size(995, 25);
+            this.lblAdvertencia.TabIndex = 1;
+            this.lblAdvertencia.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // Form1
             // 
             this.AccessibleName = "";
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1025, 397);
+            this.ClientSize = new System.Drawing.Size(1025, 431);
+            this.Controls.Add(this.lblAdvertencia);
             this.Controls.Add(this.gBox_salida);
             this.Controls.Add(this.gBox_insertar);
             this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -232,6 +250,7 @@
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Alfombras Doval Fraga Jose Angel";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.gBox_insertar.ResumeLayout(false);
             this.gBox_insertar.PerformLayout();
             this.gBox_salida.ResumeLayout(false);
@@ -258,6 +277,7 @@
         private System.Windows.Forms.ComboBox cboAlfombras;
         private System.Windows.Forms.Button btnEliminarTodas;
         private System.Windows.Forms.Button btnEliminarAlfombra;
+        private System.Windows.Forms.Label lblAdvertencia;
     }
 }
 
--- Practica02 Doval Fraga Jose Angel/Practica02 Doval Fraga Jose Angel Parte 1/Form1.cs
+++ Practica02 Doval Fraga Jose Angel/Practica02 Doval Fraga Jose Angel Parte 1/Form1.cs
@@ -10,16 +10,201 @@ using System.Windows.Forms;
 
 namespace Practica02_Doval_Fraga_Jose_Angel_Parte_1
 {
+
     public partial class Form1 : Form
     {
+        //
+        // Variables Globales
+        //
+        public bool _cajavalida = true;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void gBox_insertar_Enter(object sender, EventArgs e)
-        {
+        //
 
+
+        //
+        // Evento Boton añadir
+        //
+        private void btnAñadir_Click(object sender, EventArgs e)
+        {
+            if(txtModelo.Text !="" & txtColor.Text != "" & txtAncho.Text != "" & txtAlto.Text != "")
+            {
+
+            }
         }
+
+        //
+        // Advertencia
+        //
+        private void advertenciaDown()
+        {
+            lblAdvertencia.Visible = false;
+        }
+        private void advertenciaUp(string txt)
+        {
+            lblAdvertencia.Visible = true;
+            lblAdvertencia.BackColor = Color.OrangeRed;
+            lblAdvertencia.Text = txt;
+        }
+
+        //
+        // Metodo para comprobar si la caja esta vacia
+        //
+        private bool cajaVacia(string txt)
+        {
+            if (txt != "") return true;
+            else return false;
+        }
+
+        //
+        // Evento load
+        //
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblAdvertencia.Visible = false;
+            //gBox_insertar.Focus();
+        }
+
+        //
+        // Eventos txtModelo
+        //
+        private void txtModelo_Enter(object sender, EventArgs e)
+        {
+            if (_cajavalida)
+            {
+                txtModelo.BackColor = Color.FromArgb(160, 210, 208);
+            }
+            else
+            {
+                txtModelo.BackColor = Color.OrangeRed;
+            }
+        }
+        private void txtModelo_Leave(object sender, EventArgs e)
+        {
+            if (cajaVacia(txtModelo.Text))
+            {
+                txtModelo.BackColor = Color.White;
+                advertenciaDown();
+                _cajavalida = true;
+            }
+            else
+            {
+                txtModelo.BackColor = Color.OrangeRed;
+                advertenciaUp("Se requiere un Modelo de alfombra");
+                _cajavalida = false;
+                txtModelo.Focus();
+            }
+        }
+
+        //
+        // Eventos Color
+        //
+        private void txtColor_Enter(object sender, EventArgs e)
+        {
+            if (_cajavalida)
+            {
+                txtColor.BackColor = Color.FromArgb(160, 210, 208);
+            }
+            else
+            {
+                txtColor.BackColor = Color.OrangeRed;
+            }
+        }
+        private void txtColor_Leave(object sender, EventArgs e)
+        {
+            if (cajaVacia(txtColor.Text))
+            {
+                txtColor.BackColor = Color.White;
+                advertenciaDown();
+                _cajavalida = true;
+            }
+            else
+            {
+                txtColor.BackColor = Color.OrangeRed;
+                advertenciaUp("Se requiere un color de alfombra");
+                _cajavalida = false;
+                txtColor.Focus();
+            }
+        }
+
+        //
+        // Event Ancho
+        // 
+        private void txtAncho_Enter(object sender, EventArgs e)
+        {
+            if (_cajavalida)
+            {
+                txtAncho.BackColor = Color.FromArgb(160, 210, 208);
+            }
+            else
+            {
+                txtAncho.BackColor = Color.OrangeRed;
+            }
+        }
+        private void txtAncho_Leave(object sender, EventArgs e)
+        {
+            if (cajaVacia(txtAncho.Text) & cajaNumValida(txtAncho.Text))
+            {
+                txtAncho.BackColor = Color.White;
+                advertenciaDown();
+                _cajavalida = true;
+            }
+            else
+            {
+                txtAncho.BackColor = Color.OrangeRed;
+                advertenciaUp("Se requiere un ancho en centimetros para la alfombra");
+                _cajavalida = false;
+                txtAncho.Focus();
+            }
+        }
+
+        //
+        // Event Ancho
+        // 
+        private void txtAlto_Enter(object sender, EventArgs e)
+        {
+            if (_cajavalida)
+            {
+                txtAlto.BackColor = Color.FromArgb(160, 210, 208);
+            }
+            else
+            {
+                txtAlto.BackColor = Color.OrangeRed;
+            }
+        }
+        private void txtAlto_Leave(object sender, EventArgs e)
+        {
+            if (cajaVacia(txtAlto.Text) & cajaNumValida(txtAlto.Text))
+            {
+                txtAlto.BackColor = Color.White;
+                advertenciaDown();
+                _cajavalida = true;
+            }
+            else
+            {
+                txtAlto.BackColor = Color.OrangeRed;
+                advertenciaUp("Se requiere un alto en centimetros para la alfombra");
+                _cajavalida = false;
+                txtAlto.Focus();
+            }
+        }
+
+        //
+        // Metodos para comprobar si el valor introducido es numerico
+        //
+        private bool cajaNumValida(string txt)
+        {
+            for (int i = 0; i < txt.Length; i++)
+            {
+                char a = txt[i];
+                if (!char.IsDigit(a)) return false;
+            }
+            return true;
+        }
+
     }
 }
