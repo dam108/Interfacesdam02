3f992cbfa0e2389ed4dd8bf20dce6f71c834a604
Jose Doval <dovaldam108@gmail.com>
2020-10-12 14:52:53 +0200

    diseño de los dos programas acabados, falta el codigo

---
+++ Practica02 Doval Fraga Jose Angel/Practica02 Doval Fraga Jose Angel Parte 1/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace Practica02_Doval_Fraga_Jose_Angel_Parte_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gBox_insertar = new System.Windows.Forms.GroupBox();
+            this.gBox_salida = new System.Windows.Forms.GroupBox();
+            this.lbl_modelo = new System.Windows.Forms.Label();
+            this.lbl_color = new System.Windows.Forms.Label();
+            this.lbl_ancho = new System.Windows.Forms.Label();
+            this.lbl_cm = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_alto = new System.Windows.Forms.Label();
+            this.txtModelo = new System.Windows.Forms.TextBox();
+            this.txtColor = new System.Windows.Forms.TextBox();
+            this.txtAncho = new System.Windows.Forms.TextBox();
+            this.txtAlto = new System.Windows.Forms.TextBox();
+            this.btnAñadir = new System.Windows.Forms.Button();
+            this.cboAlfombras = new System.Windows.Forms.ComboBox();
+            this.btnInfoAlfombra = new System.Windows.Forms.Button();
+            this.btnEliminarAlfombra = new System.Windows.Forms.Button();
+            this.btnEliminarTodas = new System.Windows.Forms.Button();
+            this.gBox_insertar.SuspendLayout();
+            this.gBox_salida.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gBox_insertar
+            // 
+            this.gBox_insertar.Controls.Add(this.btnAñadir);
+            this.gBox_insertar.Controls.Add(this.txtAlto);
+            this.gBox_insertar.Controls.Add(this.txtAncho);
+            this.gBox_insertar.Controls.Add(this.txtColor);
+            this.gBox_insertar.Controls.Add(this.txtModelo);
+            this.gBox_insertar.Controls.Add(this.lbl_alto);
+            this.gBox_insertar.Controls.Add(this.label1);
+            this.gBox_insertar.Controls.Add(this.lbl_cm);
+            this.gBox_insertar.Controls.Add(this.lbl_ancho);
+            this.gBox_insertar.Controls.Add(this.lbl_color);
+            this.gBox_insertar.Controls.Add(this.lbl_modelo);
+            this.gBox_insertar.Location = new System.Drawing.Point(13, 14);
+            this.gBox_insertar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gBox_insertar.Name = "gBox_insertar";
+            this.gBox_insertar.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gBox_insertar.Size = new System.Drawing.Size(995, 162);
+            this.gBox_insertar.TabIndex = 0;
+            this.gBox_insertar.TabStop = false;
+            this.gBox_insertar.Text = "Nova alfombra";
+            this.gBox_insertar.Enter += new System.EventHandler(this.gBox_insertar_Enter);
+            // 
+            // gBox_salida
+            // 
+            this.gBox_salida.Controls.Add(this.btnEliminarTodas);
+            this.gBox_salida.Controls.Add(this.btnEliminarAlfombra);
+            this.gBox_salida.Controls.Add(this.btnInfoAlfombra);
+            this.gBox_salida.Controls.Add(this.cboAlfombras);
+            this.gBox_salida.Location = new System.Drawing.Point(13, 214);
+            this.gBox_salida.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gBox_salida.Name = "gBox_salida";
+            this.gBox_salida.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gBox_salida.Size = new System.Drawing.Size(995, 162);
+            this.gBox_salida.TabIndex = 0;
+            this.gBox_salida.TabStop = false;
+            this.gBox_salida.Text = "Alfombras dipoñibles";
+            // 
+            // lbl_modelo
+            // 
+            this.lbl_modelo.AutoSize = true;
+            this.lbl_modelo.Location = new System.Drawing.Point(28, 44);
+            this.lbl_modelo.Name = "lbl_modelo";
+            this.lbl_modelo.Size = new System.Drawing.Size(63, 21);
+            this.lbl_modelo.TabIndex = 0;
+            this.lbl_modelo.Text = "Modelo";
+            // 
+            // lbl_color
+            // 
+            this.lbl_color.AutoSize = true;
+            this.lbl_color.Location = new System.Drawing.Point(28, 100);
+            this.lbl_color.Name = "lbl_color";
+            this.lbl_color.Size = new System.Drawing.Size(35, 21);
+            this.lbl_color.TabIndex = 0;
+            this.lbl_color.Text = "Cor";
+            // 
+            // lbl_ancho
+            // 
+            this.lbl_ancho.AutoSize = true;
+            this.lbl_ancho.Location = new System.Drawing.Point(319, 100);
+            this.lbl_ancho.Name = "lbl_ancho";
+            this.lbl_ancho.Size = new System.Drawing.Size(54, 21);
+            this.lbl_ancho.TabIndex = 0;
+            this.lbl_ancho.Text = "Ancho";
+            // 
+            // lbl_cm
+            // 
+            this.lbl_cm.AutoSize = true;
+            this.lbl_cm.Location = new System.Drawing.Point(473, 100);
+            this.lbl_cm.Name = "lbl_cm";
+            this.lbl_cm.Size = new System.Drawing.Size(41, 21);
+            this.lbl_cm.TabIndex = 0;
+            this.lbl_cm.Text = "(cm)";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(685, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "(cm)";
+            // 
+            // lbl_alto
+            // 
+            this.lbl_alto.AutoSize = true;
+            this.lbl_alto.Location = new System.Drawing.Point(547, 100);
+            this.lbl_alto.Name = "lbl_alto";
+            this.lbl_alto.Size = new System.Drawing.Size(38, 21);
+            this.lbl_alto.TabIndex = 0;
+            this.lbl_alto.Text = "Alto";
+            // 
+            // txtModelo
+            // 
+            this.txtModelo.Location = new System.Drawing.Point(130, 41);
+            this.txtModelo.Name = "txtModelo";
+            this.txtModelo.Size = new System.Drawing.Size(596, 29);
+            this.txtModelo.TabIndex = 2;
+            // 
+            // txtColor
+            // 
+            this.txtColor.Location = new System.Drawing.Point(130, 92);
+            this.txtColor.Name = "txtColor";
+            this.txtColor.Size = new System.Drawing.Size(150, 29);
+            this.txtColor.TabIndex = 2;
+            // 
+            // txtAncho
+            // 
+            this.txtAncho.Location = new System.Drawing.Point(379, 92);
+            this.txtAncho.Name = "txtAncho";
+            this.txtAncho.Size = new System.Drawing.Size(88, 29);
+            this.txtAncho.TabIndex = 2;
+            // 
+            // txtAlto
+            // 
+            this.txtAlto.Location = new System.Drawing.Point(591, 92);
+            this.txtAlto.Name = "txtAlto";
+            this.txtAlto.Size = new System.Drawing.Size(88, 29);
+            this.txtAlto.TabIndex = 2;
+            // 
+            // btnAñadir
+            // 
+            this.btnAñadir.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAñadir.Location = new System.Drawing.Point(759, 41);
+            this.btnAñadir.Name = "btnAñadir";
+            this.btnAñadir.Size = new System.Drawing.Size(203, 28);
+            this.btnAñadir.TabIndex = 3;
+            this.btnAñadir.Text = "Engadir";
+            this.btnAñadir.UseVisualStyleBackColor = true;
+            // 
+            // cboAlfombras
+            // 
+            this.cboAlfombras.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboAlfombras.FormattingEnabled = true;
+            this.cboAlfombras.Location = new System.Drawing.Point(32, 46);
+            this.cboAlfombras.Name = "cboAlfombras";
+            this.cboAlfombras.Size = new System.Drawing.Size(930, 25);
+            this.cboAlfombras.TabIndex = 0;
+            // 
+            // btnInfoAlfombra
+            // 
+            this.btnInfoAlfombra.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInfoAlfombra.Location = new System.Drawing.Point(32, 99);
+            this.btnInfoAlfombra.Name = "btnInfoAlfombra";
+            this.btnInfoAlfombra.Size = new System.Drawing.Size(341, 28);
+            this.btnInfoAlfombra.TabIndex = 3;
+            this.btnInfoAlfombra.Text = "Información da alfombra";
+            this.btnInfoAlfombra.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminarAlfombra
+            // 
+            this.btnEliminarAlfombra.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminarAlfombra.Location = new System.Drawing.Point(379, 99);
+            this.btnEliminarAlfombra.Name = "btnEliminarAlfombra";
+            this.btnEliminarAlfombra.Size = new System.Drawing.Size(203, 28);
+            this.btnEliminarAlfombra.TabIndex = 3;
+            this.btnEliminarAlfombra.Text = "Eliminar alfombra";
+            this.btnEliminarAlfombra.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminarTodas
+            // 
+            this.btnEliminarTodas.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminarTodas.Location = new System.Drawing.Point(588, 99);
+            this.btnEliminarTodas.Name = "btnEliminarTodas";
+            this.btnEliminarTodas.Size = new System.Drawing.Size(374, 28);
+            this.btnEliminarTodas.TabIndex = 3;
+            this.btnEliminarTodas.Text = "Eliminar todas";
+            this.btnEliminarTodas.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AccessibleName = "";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1025, 397);
+            this.Controls.Add(this.gBox_salida);
+            this.Controls.Add(this.gBox_insertar);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Alfombras Doval Fraga Jose Angel";
+            this.gBox_insertar.ResumeLayout(false);
+            this.gBox_insertar.PerformLayout();
+            this.gBox_salida.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gBox_insertar;
+        private System.Windows.Forms.GroupBox gBox_salida;
+        private System.Windows.Forms.TextBox txtAncho;
+        private System.Windows.Forms.TextBox txtColor;
+        private System.Windows.Forms.TextBox txtModelo;
+        private System.Windows.Forms.Label lbl_alto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_cm;
+        private System.Windows.Forms.Label lbl_ancho;
+        private System.Windows.Forms.Label lbl_color;
+        private System.Windows.Forms.Label lbl_modelo;
+        private System.Windows.Forms.TextBox txtAlto;
+        private System.Windows.Forms.Button btnAñadir;
+        private System.Windows.Forms.Button btnInfoAlfombra;
+        private System.Windows.Forms.ComboBox cboAlfombras;
+        private System.Windows.Forms.Button btnEliminarTodas;
+        private System.Windows.Forms.Button btnEliminarAlfombra;
+    }
+}
+
---
+++ Practica02 Doval Fraga Jose Angel/Practica02 Doval Fraga jose Angel Parte 2/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Practica02_Doval_Fraga_jose_Angel_Parte_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gBox_info = new System.Windows.Forms.GroupBox();
+            this.gBoxGestion = new System.Windows.Forms.GroupBox();
+            this.lblProvincia = new System.Windows.Forms.Label();
+            this.lblLocalidad_info = new System.Windows.Forms.Label();
+            this.cboProvincia = new System.Windows.Forms.ComboBox();
+            this.cboLocalidad = new System.Windows.Forms.ComboBox();
+            this.radEliminar = new System.Windows.Forms.RadioButton();
+            this.radModificar = new System.Windows.Forms.RadioButton();
+            this.radAgregar = new System.Windows.Forms.RadioButton();
+            this.lblLocalidad_gestion = new System.Windows.Forms.Label();
+            this.txtLocalidad_gestion = new System.Windows.Forms.TextBox();
+            this.btnEjecutar = new System.Windows.Forms.Button();
+            this.gBox_info.SuspendLayout();
+            this.gBoxGestion.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gBox_info
+            // 
+            this.gBox_info.Controls.Add(this.cboLocalidad);
+            this.gBox_info.Controls.Add(this.cboProvincia);
+            this.gBox_info.Controls.Add(this.lblLocalidad_info);
+            this.gBox_info.Controls.Add(this.lblProvincia);
+            this.gBox_info.Location = new System.Drawing.Point(12, 12);
+            this.gBox_info.Name = "gBox_info";
+            this.gBox_info.Size = new System.Drawing.Size(635, 148);
+            this.gBox_info.TabIndex = 0;
+            this.gBox_info.TabStop = false;
+            this.gBox_info.Text = "Información";
+            // 
+            // gBoxGestion
+            // 
+            this.gBoxGestion.Controls.Add(this.btnEjecutar);
+            this.gBoxGestion.Controls.Add(this.txtLocalidad_gestion);
+            this.gBoxGestion.Controls.Add(this.radAgregar);
+            this.gBoxGestion.Controls.Add(this.radModificar);
+            this.gBoxGestion.Controls.Add(this.lblLocalidad_gestion);
+            this.gBoxGestion.Controls.Add(this.radEliminar);
+            this.gBoxGestion.Location = new System.Drawing.Point(12, 188);
+            this.gBoxGestion.Name = "gBoxGestion";
+            this.gBoxGestion.Size = new System.Drawing.Size(635, 211);
+            this.gBoxGestion.TabIndex = 1;
+            this.gBoxGestion.TabStop = false;
+            this.gBoxGestion.Text = "Gestión de información";
+            // 
+            // lblProvincia
+            // 
+            this.lblProvincia.AutoSize = true;
+            this.lblProvincia.Location = new System.Drawing.Point(22, 34);
+            this.lblProvincia.Name = "lblProvincia";
+            this.lblProvincia.Size = new System.Drawing.Size(74, 21);
+            this.lblProvincia.TabIndex = 0;
+            this.lblProvincia.Text = "Provincia";
+            // 
+            // lblLocalidad_info
+            // 
+            this.lblLocalidad_info.AutoSize = true;
+            this.lblLocalidad_info.Location = new System.Drawing.Point(22, 81);
+            this.lblLocalidad_info.Name = "lblLocalidad_info";
+            this.lblLocalidad_info.Size = new System.Drawing.Size(76, 21);
+            this.lblLocalidad_info.TabIndex = 0;
+            this.lblLocalidad_info.Text = "Localidad";
+            // 
+            // cboProvincia
+            // 
+            this.cboProvincia.FormattingEnabled = true;
+            this.cboProvincia.Location = new System.Drawing.Point(118, 31);
+            this.cboProvincia.Name = "cboProvincia";
+            this.cboProvincia.Size = new System.Drawing.Size(495, 29);
+            this.cboProvincia.TabIndex = 1;
+            // 
+            // cboLocalidad
+            // 
+            this.cboLocalidad.FormattingEnabled = true;
+            this.cboLocalidad.Location = new System.Drawing.Point(118, 78);
+            this.cboLocalidad.Name = "cboLocalidad";
+            this.cboLocalidad.Size = new System.Drawing.Size(495, 29);
+            this.cboLocalidad.TabIndex = 1;
+            // 
+            // radEliminar
+            // 
+            this.radEliminar.AutoSize = true;
+            this.radEliminar.Location = new System.Drawing.Point(26, 42);
+            this.radEliminar.Name = "radEliminar";
+            this.radEliminar.Size = new System.Drawing.Size(243, 25);
+            this.radEliminar.TabIndex = 0;
+            this.radEliminar.TabStop = true;
+            this.radEliminar.Text = "Eliminar localidad seleccionada";
+            this.radEliminar.UseVisualStyleBackColor = true;
+            // 
+            // radModificar
+            // 
+            this.radModificar.AutoSize = true;
+            this.radModificar.Location = new System.Drawing.Point(26, 73);
+            this.radModificar.Name = "radModificar";
+            this.radModificar.Size = new System.Drawing.Size(252, 25);
+            this.radModificar.TabIndex = 0;
+            this.radModificar.TabStop = true;
+            this.radModificar.Text = "Modificar localidad seleccionada";
+            this.radModificar.UseVisualStyleBackColor = true;
+            // 
+            // radAgregar
+            // 
+            this.radAgregar.AutoSize = true;
+            this.radAgregar.Location = new System.Drawing.Point(26, 104);
+            this.radAgregar.Name = "radAgregar";
+            this.radAgregar.Size = new System.Drawing.Size(139, 25);
+            this.radAgregar.TabIndex = 0;
+            this.radAgregar.TabStop = true;
+            this.radAgregar.Text = "Nueva localidad";
+            this.radAgregar.UseVisualStyleBackColor = true;
+            // 
+            // lblLocalidad_gestion
+            // 
+            this.lblLocalidad_gestion.AutoSize = true;
+            this.lblLocalidad_gestion.Location = new System.Drawing.Point(22, 159);
+            this.lblLocalidad_gestion.Name = "lblLocalidad_gestion";
+            this.lblLocalidad_gestion.Size = new System.Drawing.Size(76, 21);
+            this.lblLocalidad_gestion.TabIndex = 0;
+            this.lblLocalidad_gestion.Text = "Localidad";
+            // 
+            // txtLocalidad_gestion
+            // 
+            this.txtLocalidad_gestion.Location = new System.Drawing.Point(104, 156);
+            this.txtLocalidad_gestion.Name = "txtLocalidad_gestion";
+            this.txtLocalidad_gestion.Size = new System.Drawing.Size(509, 29);
+            this.txtLocalidad_gestion.TabIndex = 1;
+            // 
+            // btnEjecutar
+            // 
+            this.btnEjecutar.Location = new System.Drawing.Point(307, 42);
+            this.btnEjecutar.Name = "btnEjecutar";
+            this.btnEjecutar.Size = new System.Drawing.Size(306, 87);
+            this.btnEjecutar.TabIndex = 2;
+            this.btnEjecutar.Text = "Ejecutar";
+            this.btnEjecutar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 423);
+            this.Controls.Add(this.gBoxGestion);
+            this.Controls.Add(this.gBox_info);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Localidades Doval Fraga Jose Angel";
+            this.gBox_info.ResumeLayout(false);
+            this.gBox_info.PerformLayout();
+            this.gBoxGestion.ResumeLayout(false);
+            this.gBoxGestion.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gBox_info;
+        private System.Windows.Forms.ComboBox cboLocalidad;
+        private System.Windows.Forms.ComboBox cboProvincia;
+        private System.Windows.Forms.Label lblLocalidad_info;
+        private System.Windows.Forms.Label lblProvincia;
+        private System.Windows.Forms.GroupBox gBoxGestion;
+        private System.Windows.Forms.Button btnEjecutar;
+        private System.Windows.Forms.TextBox txtLocalidad_gestion;
+        private System.Windows.Forms.RadioButton radAgregar;
+        private System.Windows.Forms.RadioButton radModificar;
+        private System.Windows.Forms.Label lblLocalidad_gestion;
+        private System.Windows.Forms.RadioButton radEliminar;
+    }
+}
+
